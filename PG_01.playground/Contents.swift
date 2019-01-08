e6f87b2963eb1ced8a18b9ca336d11bd7248be0d
Gustavo Ureta <5447557+ginogustavo@users.noreply.github.com>
2019-01-07 22:13:53 -0500

    Adding PlayGround 1

---
+++ PG_01.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var demo:String = "Hello world"
+print(demo)
+
+var firstName, lastName, middleName:String!
+firstName = "Gino"
+lastName =  "Ureta"
+middleName = "Gustavo"
+
+print(firstName)
+
+var a:Float!
+var pi1_explicit:Double = 3.141516
+var pi2_implicit = 3.141516
+var c:Int!
+
+
+
+func saludando(){
+    print("Saludo: Hey!!!!")
+}
+
+saludando()
