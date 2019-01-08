b94fd372b48e7608157abc62c37a25605785f3ee
Gustavo Ureta <5447557+ginogustavo@users.noreply.github.com>
2019-01-07 22:11:22 -0500

    Adding Project 1

---
+++ Sample01/Sample01/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Sample01
+//
+//  Created by Academia on 7/01/19.
+//  Copyright © 2019 Index. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var txtNombre: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Testing TXT")
+        
+    }
+
+
+}
+
